75a3e4927ff16c1e4c6213e1cdf38149bfadb0d0
Luo <gnulinux@126.com>
2016-07-12 08:40:53 +0800

    update src/playlist.rs FromStr Trait

--- src/playlist.rs
+++ src/playlist.rs
@@ -63,13 +63,35 @@ impl ToString for Tag {
     }
 }
 
+
+
 impl FromStr for Tag {
     type Err = ();
     fn from_str(s: &str) -> Result<Tag, ()> {
         match s {
-            "EXTM3U" => Ok(Tag::M3U),
-            "EXT-X-VERSION" => Ok(Tag::VERSION),
-            _ => Err(())
+            "EXTM3U"            => Ok(Tag::M3U),
+            "EXT-X-VERSION"     => Ok(Tag::VERSION),
+            "EXTINF"            => Ok(Tag::INF),
+            "EXT-X-BYTERANGE"   => Ok(Tag::BYTERANGE),
+            "EXT-X-DISCONTINUITY"       =>Ok(Tag::DISCONTINUITY),
+            "EXT-X-KEY"                 => Ok(Tag::KEY),
+            "EXT-X-MAP"                 => Ok(Tag::MAP),
+            "EXT-X-PROGRAM-DATE-TIME"   => Ok(Tag::PROGRAM_DATE_TIME),
+            "EXT-X-DATERANGE"           => Ok(Tag::DATERANGE),
+            "EXT-X-TARGETDURATION"      => Ok(Tag::TARGETDURATION),
+            "EXT-X-MEDIA-SEQUENCE"      => Ok(Tag::MEDIA_SEQUENCE),
+            "EXT-X-DISCONTINUITY-SEQUENCE" => Ok(Tag::DISCONTINUITY_SEQUENCE),
+            "EXT-X-ENDLIST"                => Ok(Tag::ENDLIST),
+            "EXT-X-PLAYLIST-TYPE"          => Ok(Tag::PLAYLIST_TYPE),
+            "EXT-X-I-FRAMES-ONLY"          => Ok(Tag::I_FRAMES_ONLY),
+            "EXT-X-MEDIA"                  => Ok(Tag::MEDIA),
+            "EXT-X-STREAM-INF"             => Ok(Tag::STREAM_INF),
+            "EXT-X-I-FRAME-STREAM-INF"     => Ok(Tag::I_FRAME_STREAM_INF),
+            "EXT-X-SESSION-DATA"           => Ok(Tag::SESSION_DATA),
+            "EXT-X-SESSION-KEY"            => Ok(Tag::SESSION_KEY),
+            "EXT-X-INDEPENDENT-SEGMENTS"   => Ok(Tag::INDEPENDENT_SEGMENTS),
+            "EXT-X-START"                  => Ok(Tag::START),
+            _                              => Err(())
         }
     }
 }
@@ -77,7 +99,7 @@ impl FromStr for Tag {
 
 #[derive(Debug)]
 pub struct Playlist {
-    map: BTreeMap<>
+    // map: BTreeMap<>
 }
 
 impl Playlist {
